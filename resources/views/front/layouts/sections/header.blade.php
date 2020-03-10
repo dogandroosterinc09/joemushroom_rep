c82e75da3b18a6fe82ca60e56d0c38d440929483
Philip James Sabio <psab72@gmail.com>
2020-03-10 13:39:23 +0800

    updates

--- resources/views/front/layouts/sections/header.blade.php
+++ resources/views/front/layouts/sections/header.blade.php
@@ -1,108 +1,177 @@
 <header>
 
-    <a class="navbar-brand" href="{{ url('/') }}">
-      <img src="{{ asset('public/images/capitol-logo-colored.png') }}" alt="logo">
-    </a>
-
-    <nav class="navbar navbar-expand-md navbar-dark sticky-top">
-      <!-- One of the primary actions on mobile is to call a business - This displays a phone button on mobile only -->
-      <div class="navbar-toggler-right">
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
+  {{-- FOR DESKTOP MENU  --}}
+  <div class="main-nagivation-desktop container">
+    <div class="main-nagivation-desktop__wrapper--row row">
+      <div class="col-lg-4">
+        <a href="{{ url('/') }}">
+          <img src="{{ asset('public/images/header/logo.jpg') }}" alt="logo">
+        </a>
       </div>
-
-
-      <div class="collapse navbar-collapse flex-column " id="navbar">
-
-        <ul class="navbar-nav navbar-nav--top w-100 justify-content-end">
-          <li class="nav-item">
-            <a class="nav-link" href="#"><i class="fas fa-user"></i> Log in</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#"><i class="fas fa-info-circle"></i> Attendee Info</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="tel:(800) 450-5012"><i class="fas fa-phone"></i> (800) 450-5012</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link text-uppercase" href="#">Request Info</a>
-          </li>
-          <li class="nav-item register">
-            <a class="nav-link text-uppercase" href="#">Register Today!</a>
-          </li>
-
-        </ul>
-
-        <ul class="navbar-nav navbar-nav--bottom justify-content-end w-100">
-          <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" href="http://example.com" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-              Find a Camp
-            </a>
-            <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-              <a class="dropdown-item" href="#">Action</a>
-              <a class="dropdown-item" href="#">Another action</a>
-              <a class="dropdown-item" href="#">Something else here</a>
-            </div>
-          </li>
-          <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" href="http://example.com" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-              Camp Programs
-            </a>
-            <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-              <a class="dropdown-item" href="#">Action</a>
-              <a class="dropdown-item" href="#">Another action</a>
-              <a class="dropdown-item" href="#">Something else here</a>
-            </div>
-          </li>
-          <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" href="http://example.com" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-              Year-round Debate
-            </a>
-            <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-              <a class="dropdown-item" href="#">Action</a>
-              <a class="dropdown-item" href="#">Another action</a>
-              <a class="dropdown-item" href="#">Something else here</a>
-            </div>
-          </li>
-          <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" href="http://example.com" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-              About Us
-            </a>
-            <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-              <a class="dropdown-item" href="#">Action</a>
-              <a class="dropdown-item" href="#">Another action</a>
-              <a class="dropdown-item" href="#">Something else here</a>
-            </div>
-          </li>
-        </ul>
-
-
+      <div class="col-lg-8">
+        <nav class="navbar-bar">
+          <ul class="navbar-bar__wrapper">
+            <li class="nav-item">
+              <a class="nav-link" href="{{ url('/') }}">Home <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="{{ url('contact-us') }}">Contact Us</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="{{ url('about-us') }}">About Us</a>
+            </li>
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle"
+                 href="{{ route('customer.login') }}" {{--data-toggle="dropdown"--}}>
+                Login
+              </a>
+              <div class="dropdown-menu">
+                <ul class="sub-menu">
+                  <li>
+                    <a class="nav-link" href="{{ route('customer.register') }}"> Register</a>
+                  </li>
+                </ul>
+              </div>
+            </li>
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="#" {{--data-toggle="dropdown"--}}>
+                Dropdown
+              </a>
+              <div class="dropdown-menu">
+                <ul class="sub-menu">
+                  <li>
+                    <a class="nav-link" href="#"> Sub menu 1</a>
+                  </li>
+                  <li>
+                    <a class="nav-link" href="#"> Sub menu 1</a>
+                  </li>
+                  <li>
+                    <a class="nav-link" href="#"> Sub menu 1</a>
+                  </li>
+                </ul>
+              </div>
+            </li>
+          </ul>
+        </nav>
       </div>
+      <!-- ending of row  -->
+    </div>
+    <!-- ending of container  -->
+  </div>
 
-    </nav>
+
+  {{-- FOR MOBILE MENU  --}}
+  <div class="main-nagivation-mobile">
+    <div class="main-nagivation-mobile__wrapper container">
+      <div class="main-nagivation-mobile__wrapper--row">
+
+        <div class="col-md-12">
+        <!-- {{-- MOBILE HEADER --}} -->
+          <div class="mobile-header__wrapper">
+
+            <div class="mobile-logo">
+              <a href="{{ url('/') }}">
+                <img src="{{ asset('public/images/header/logo.jpg') }}" alt="logo">
+              </a>
+            </div>
+
+
+            <div class="mobile-header__wrapper--item d-inline-block align-top">
+              <div class="mob-burger-menu">
+                <div class="bar1"></div>
+                <div class="bar2"></div>
+                <div class="bar3"></div>
+              </div>
+              <div class="mob-nav-menu">
+                <a href="javascript:void()" class="btn btn--close-menu"> <i class="fa fa-times"
+                                                                            aria-hidden="true"></i> </a>
+                <ul>
+                  <!-- **************************************** -->
+                  <li class="nav-item">
+                    <a class="nav-link" href="{{ url('/') }}">Home <span
+                              class="sr-only">(current)</span></a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="{{ url('contact-us') }}">Contact Us</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="{{ url('about-us') }}">About Us</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="{{ route('customer.login') }}">
+                      Login
+                    </a>
+
+                    <div class="icon-button icon-button__open">
+                      <i class="fa fa-plus" aria-hidden="true"></i>
+                    </div>
+                    <div class="icon-button icon-button__close">
+                      <i class="fa fa-angle-down" aria-hidden="true"></i>
+                    </div>
+
+                    <ul class="sub-menu no-mega-sub">
+                      <li>
+                        <a href="{{ route('customer.register') }}"> Register</a>
+                      </li>
+                    </ul>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link " href="#">
+                      Dropdown
+                    </a>
+
+                    <div class="icon-button icon-button__open">
+                      <i class="fa fa-plus" aria-hidden="true"></i>
+                    </div>
+                    <div class="icon-button icon-button__close">
+                      <i class="fa fa-angle-down" aria-hidden="true"></i>
+                    </div>
+
+                    <ul class="sub-menu no-mega-sub">
+                      <li>
+                        <a href="#"> Sub menu 1</a>
+                      </li>
+                      <li>
+                        <a href="#"> Sub menu 1</a>
+                      </li>
+                      <li>
+                        <a href="#"> Sub menu 1</a>
+                      </li>
+                    </ul>
+                  </li>
+                  <!-- **************************************** -->
+
+                </ul>
+              </div>
+            </div>
+          </div>
+
+
+        </div>
+      </div>
+    </div>
 
 </header>
 
 
-{{-- <section class="search-box">
-    <div class="search-box__button"></div>
-    <div class="search-box__wrapper container">
-        <div class="row">
-            <div class="col-md-12">
-                <form class="search-form" action="{{ url('/') }}" method="GET">
-                    <div class="form-group">
-                        <a class="nav-link " type="submit" href="javascript:void(0)">
-                            <i class="fa fa-search"></i>
-                        </a>
-                        <input type="text" name="keyword" id="keyword" placeholder="Search Keyword">
+<section class="search-box">
+  <div class="search-box__button"></div>
+  <div class="search-box__wrapper container">
+    <div class="row">
+      <div class="col-md-12">
+        <form class="search-form" action="{{ url('/') }}" method="GET">
+          <div class="form-group">
+            <a class="nav-link " type="submit" href="javascript:void(0)">
+              <i class="fa fa-search"></i>
+            </a>
+            <input type="text" name="keyword" id="keyword" placeholder="Search Keyword">
 
-                        <input type="text" name="keyword" id="keyword" placeholder="Search Keyword"
-                                   value="{{ !empty($search_params) && isset($search_params['keyword']) ? $search_params['keyword'] : '' }}"
-                        spellcheck="false">
-                    </div>
-                </form>
-            </div>
-        </div>
+            {{-- <input type="text" name="keyword" id="keyword" placeholder="Search Keyword"
+                       value="{{ !empty($search_params) && isset($search_params['keyword']) ? $search_params['keyword'] : '' }}"
+            spellcheck="false"> --}}
+          </div>
+        </form>
+      </div>
     </div>
-</section> --}}
+  </div>
+</section>
--- resources/views/front/pages/custom-page/home.blade.php
+++ resources/views/front/pages/custom-page/home.blade.php
@@ -1,1 +1,154 @@
-Home page
+<section class="homepage homepage--main">
+    @include('front.layouts.sections.header')
+{{--    @include('front.pages.custom-page.sections.slider')--}}
+    <main class="main-content">
+        {{-- individual section  --}}
+        <section class="section-name">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-6 section-name__item">
+                        test message
+                    </div>
+                    <div class="col-md-6 section-name__item">
+                        test message
+                    </div>
+                </div>
+            </div> {{-- end of default-content--row --}}
+        </section> {{-- end of default-content --}}
+
+
+
+
+        <section class="">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-12">
+
+                        {{-- "global-featured-slider --}}
+                        <div class="global-featured-slider">
+                            <div class="container">
+                                <div class="row">
+                                    <div class="col-lg-12 global-featured-slider__top">
+
+                                        <div class="featured-slider-main featured-slider-main__properties">
+
+                                            {{-- slick  --}}
+                                            <div class="global-featured-slider__top--slick">
+
+                                  {{--              <div class="global-featured-slider__item">
+                                                    <a href="#">
+                                                        <div class="global-featured-slider__item--image image-background">
+                                                            <img src="{!! !empty($item) && !empty($item->page_sections()->where('section', 'banner_image')->first()) ?
+                                                                            asset($item->page_sections()->where('section', 'banner_image')->first()->content) :
+                                                                            asset('public/images/dogandrooster_full_bg.jpg') !!}">
+                                                        </div>
+                                                    </a>
+                                                    <div class="global-featured-slider__description">
+                                                        <h2>slider name</h2>
+                                                        <p>test</p>
+                                                    </div>
+                                                </div>
+                                                <div class="global-featured-slider__item">
+                                                    <a href="#">
+                                                        <div class="global-featured-slider__item--image image-background">
+                                                            <img src="{!! !empty($item) && !empty($item->page_sections()->where('section', 'banner_image')->first()) ?
+                                                                            asset($item->page_sections()->where('section', 'banner_image')->first()->content) :
+                                                                            asset('public/images/dogandrooster_full_bg.jpg') !!}">
+                                                        </div>
+                                                    </a>
+                                                    <div class="global-featured-slider__description">
+                                                        <h2>slider name</h2>
+                                                        <p>test</p>
+                                                    </div>
+                                                </div>
+                                                <div class="global-featured-slider__item">
+                                                    <a href="#">
+                                                        <div class="global-featured-slider__item--image image-background">
+                                                            <img src="{!! !empty($item) && !empty($item->page_sections()->where('section', 'banner_image')->first()) ?
+                                                                            asset($item->page_sections()->where('section', 'banner_image')->first()->content) :
+                                                                            asset('public/images/dogandrooster_full_bg.jpg') !!}">
+                                                        </div>
+                                                    </a>
+                                                    <div class="global-featured-slider__description">
+                                                        <h2>slider name</h2>
+                                                        <p>test</p>
+                                                    </div>
+                                                </div>
+                                                <div class="global-featured-slider__item">
+                                                    <a href="#">
+                                                        <div class="global-featured-slider__item--image image-background">
+                                                            <img src="{!! !empty($item) && !empty($item->page_sections()->where('section', 'banner_image')->first()) ?
+                                                                            asset($item->page_sections()->where('section', 'banner_image')->first()->content) :
+                                                                            asset('public/images/dogandrooster_full_bg.jpg') !!}">
+                                                        </div>
+                                                    </a>
+                                                    <div class="global-featured-slider__description">
+                                                        <h2>slider name</h2>
+                                                        <p>test</p>
+                                                    </div>
+                                                </div>--}}
+
+
+                                                {{-- end of featured-slider-main__item --}}
+                                            </div>
+                                            {{-- slick  --}}
+
+                                        </div>
+                                    </div>
+
+
+                                    <div class="col-lg-12 global-featured-slider__bottom">
+                                        {{-- slick  --}}
+                                        <div class="global-featured-slider__bottom--slick">
+
+                                          {{--  <div class="global-featured-slider__item">
+                                                <div class="global-featured-slider__item--image image-background">
+                                                    <img src="{!! !empty($item) && !empty($item->page_sections()->where('section', 'banner_image')->first()) ?
+                                                                        asset($item->page_sections()->where('section', 'banner_image')->first()->content) :
+                                                                        asset('public/images/dogandrooster_full_bg.jpg') !!}">
+                                                </div>
+                                            </div>
+                                            <div class="global-featured-slider__item">
+                                                <div class="global-featured-slider__item--image image-background">
+                                                    <img src="{!! !empty($item) && !empty($item->page_sections()->where('section', 'banner_image')->first()) ?
+                                                                        asset($item->page_sections()->where('section', 'banner_image')->first()->content) :
+                                                                        asset('public/images/dogandrooster_full_bg.jpg') !!}">
+                                                </div>
+                                            </div>
+                                            <div class="global-featured-slider__item">
+                                                <div class="global-featured-slider__item--image image-background">
+                                                    <img src="{!! !empty($item) && !empty($item->page_sections()->where('section', 'banner_image')->first()) ?
+                                                                        asset($item->page_sections()->where('section', 'banner_image')->first()->content) :
+                                                                        asset('public/images/dogandrooster_full_bg.jpg') !!}">
+                                                </div>
+                                            </div>
+                                            <div class="global-featured-slider__item">
+                                                <div class="global-featured-slider__item--image image-background">
+                                                    <img src="{!! !empty($item) && !empty($item->page_sections()->where('section', 'banner_image')->first()) ?
+                                                                        asset($item->page_sections()->where('section', 'banner_image')->first()->content) :
+                                                                        asset('public/images/dogandrooster_full_bg.jpg') !!}">
+                                                </div>
+                                            </div>--}}
+
+
+                                            {{-- end of featured-slider-main__item --}}
+                                        </div>
+                                        {{-- slick  --}}
+                                    </div>
+
+                                </div>
+                            </div>
+                        </div>
+                        {{-- "global-featured-slider --}}
+
+
+                    </div>
+                </div>
+            </div>
+        </section>
+
+
+
+    </main>
+    @include('front.layouts.sections.footer')
+</section>
